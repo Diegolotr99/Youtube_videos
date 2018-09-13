4b7a386db4441d98aced57ed3f5cc1364ebb2c0e
Dennis Córdoba <dennis.cordoba@gmail.com>
2018-09-13 16:19:12 -0600

    V1

---
+++ V1/PuntoVentaPulperia/PulperiaEntidades/Papaya.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PulperiaEntidades
+{
+    public class Papaya
+    {
+        public string Color { get; set; }
+    }
+}
