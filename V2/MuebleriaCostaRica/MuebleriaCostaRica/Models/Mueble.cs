f71bdd35a7441f2d2d73d561546cf5e1d128a39c
Dennis Córdoba <dennis.cordoba@gmail.com>
2018-09-20 16:45:24 -0600

    Video 2, web api
    Signed-off-by: Dennis Córdoba <dennis.cordoba@gmail.com>

---
+++ V2/MuebleriaCostaRica/MuebleriaCostaRica/Models/Mueble.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MuebleriaCostaRica.Models
+{
+    public class Mueble
+    {
+        public string Codigo { get; set; }
+        public string Nombre { get; set; }
+        public string Tipo { get; set; }
+        public double Precio { get; set; }
+    }
+}
